44d5a2dc593a7506138c7fdee5ea7ce447906061
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 12:44:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index{|word, i| word << 's' if i!=1}.map{|w| w}
+  puts array.each_with_index{|word, i| word << 's' if i!=1}.map{|w| w}
 end
